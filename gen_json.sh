7e43888f5be037debc33ad9558d2d0ac0f0cefa0
Luu Nguyen <47746985+luunguyen97@users.noreply.github.com>
2022-01-07 15:52:53 +0700

    Add files via upload

--- gen_json.sh
+++ gen_json.sh
@@ -1,4 +1,4 @@
 echo "GENERATE JSON ANNOTATION"
 python gen_json.py \
-       --dataset_dir /home/luunp/HDD/Dataset/Tracker_datasets/val_dataset \
-       --json_anno /home/luunp/HDD/Dataset/Tracker_datasets/val_dataset/val.json
+       --dataset_dir /home/luunp/HDD/Dataset/Tracker_datasets/val_dataset/QDT_TIR \
+       --json_anno /home/luunp/HDD/Dataset/Tracker_datasets/val_dataset/ADT/TIR/QDT_TIR.json
